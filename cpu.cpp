d95c24d12bef6d15e6bfc4e3af57fdadaa8e2210
kishore-ganesh <kishore2912000@gmail.com>
2019-07-28 14:11:04 +0530

    Added flags to first batch of instructions and started adding second batch
    Signed-off-by: kishore-ganesh <kishore2912000@gmail.com>

--- cpu.cpp
+++ cpu.cpp
@@ -1,5 +1,7 @@
 #include<stdio.h>
 /* RUN SET FLAGS AFTER RUNNING INSTRUCTION */
+/*We should handle addresses automaitcally or not? */
+//Check write addresses
 struct iNES_Header{
     char NESConstant[4];
     char prgSize;
@@ -9,7 +11,15 @@ struct iNES_Header{
 };
 class NES{
     char A, X, Y, P, SP; //check this
-
+    enum masks{
+        CARRY = 0x01,
+        ZERO = 0x02,
+        INT = 0x04,
+       // DEC = 0x08,
+        B = 0x10,
+        OVERFLOW = 0x20,
+        NEGATIVE = 0x40
+    } flagMask;
     /*
     A - Accumulator
     X, Y - Index register
@@ -53,13 +63,62 @@ class NES{
             fread(&PRG_ROM[0x4000], 0x4000, 1, rom);
         }
     }
+
+    void setFlag(char mask, bool bit){
+        P&=!mask;
+        if(bit){
+            P|=mask;
+        }
+    }
+
+    void checkValueFlags(char value){
+        bool negBit, zeroBit;
+        negBit = value < 0 ? 1: 0;
+        zeroBit = value = 0? 0: 1;
+        setFlag(ZERO, zeroBit);
+        setFlag(NEGATIVE, negBit);
+    }
+
+    void readImmediate(unsigned short& PC, char& data, unsigned short address){
+        PC = PC + 1;
+        address = PC;
+        data = readAddress(PC);
+    }
+
+    void readZeroPage(unsigned short& PC, char& data, unsigned short address){
+        PC = PC + 1;
+        address = readAddress(PC);
+        data = readAddress(address);
+    }
+
+    void readAbsolute(unsigned short& PC, char& data, unsigned short &address){
+        PC = PC + 1;
+        address = readLittleEndian(PC);
+        PC = PC + 1;
+        data = readAddress(address);
+    }
+
+    /* ZERO PAGE, X: X provided since in two instructions the X changes to Y */
+    void readZeroPageX(unsigned short& PC, char& data, unsigned short& address, char X){
+        PC = PC + 1;
+        address = PC + X;
+        data = readAddress(PC+X);
+    }
+
+    void readAbsoluteX(unsigned short &PC, char& data, unsigned short & address,char X){
+        PC = PC + 1;
+        address = readLittleEndian(PC) + X;
+        PC = PC + 1;
+        data = readAddress(address);
+    }
     void processInstruction(char instruction){
         
+        char data = 0;
+        unsigned short address;
+        //check specific instructions here then pattern matching
         if(instruction&0x01){
-            char data = 0;
-            short address;
             /* Check addressing modes */
-            switch(instruction&0x1E0){
+            switch(instruction&0x1C){
                 case 0x0: {
                     PC = PC + 1;
                     address = readLittleEndian(PC+X);
@@ -68,49 +127,33 @@ class NES{
                     break;
                 } 
                 case 0x1: {
-                    PC = PC + 1;
-                    data = readAddress(PC);
-                    data = readAddress(data);
+                    readZeroPage(PC, data, address);
                     break;
                 }
                 case 0x2:{
-                    PC = PC + 1;
-                    data = readAddress(PC);
+                    readImmediate(PC, data, address);
                     break;
                 }
                 case 0x3: {
-                    short address;
-                    PC = PC + 1;
-                    address = readLittleEndian(PC);
-                    PC = PC + 1;
-                    data = readAddress(address);
+                    readAbsolute(PC, data, address);
                     break;
                 }
                 case 0x4: {
                     PC = PC + 1;
-                    short address = readLittleEndian(PC);
+                    short address = readLittleEndian(PC)+Y;
                     PC = PC + 1;
-                    data = readAddress(address+Y);
+                    data = readAddress(address);
                     break;
                 }
                 case 0x5: {
-                    PC = PC + 1;
-                    data = readAddress(PC+X);
+                    readZeroPageX(PC, data, address, X);
                 }
                 case 0x6: { 
-                    PC = PC + 1;
-                    short int address;
-                    address = readLittleEndian(PC);
-                    PC = PC + 1;
-                    data = readAddress(address+X);
+                    readAbsoluteX(PC, data, address, X);
                     break;
                 }
                 case 0x7: {
-                    short int address;
-                    PC = PC + 1;
-                    address = readLittleEndian(PC);
-                    PC = PC + 1;
-                    data = readAddress(address+Y);
+                    readAbsoluteX(PC, data, address, Y); /*read absolute Y */
                     break;
                 }
             }
@@ -120,10 +163,60 @@ class NES{
                 case 0x1: AND(data); break;
                 case 0x2: EOR(data); break; 
                 case 0x3: ADC(data); break;
-                case 0x4: STA(address); PC = PC -1; break;  
+                case 0x4: STA(address); break;  //removed PC = PC -1
                 case 0x5: LDA(data); break;
                 case 0x6: CMP(data); break;
                 case 0x7: SBC(data); break;
+            }
+        }
+        else if(instruction&0x02){
+            bool accumulator = false;
+            switch(instruction&0x1C){ // check this mask
+                case 0x0: {
+                    readImmediate(PC, data, address);
+                    break;
+                }
+                case 0x1: {
+                    readZeroPage(PC, data, address);
+                    break;
+                }
+                case 0x2: {
+                    accumulator = true;
+                    //Accumulator
+                    break;
+                }
+                case 0x3: {
+                    readAbsolute(PC, data, address);
+                    break;
+                } 
+                case 0x5: {
+                    if(instruction&0xE0==0x4||instruction&0xE0==0x5){   //check mask
+                        readZeroPageX(PC, data, address, Y);
+                    }
+                    else{
+                        readZeroPageX(PC, data, address, X);
+                    }
+                    break;
+                }
+                case 0x7: {
+                    if(instruction&0xE0==0x5){
+                        readAbsoluteX(PC, data, address, Y);
+                    }
+                    else{
+                        readAbsoluteX(PC, data, address, X);
+                    }
+                    break;
+                }
+            }
+            switch(instruction&0xE0){
+                case 0x0: ASL(address, accumulator); break;
+                case 0x1: ROL(address, accumulator); break;
+                case 0x2: LSR(address, accumulator); break; 
+                case 0x3: ROR(address, accumulator); break;
+                case 0x4: STX(address, accumulator); break;
+                case 0x5: LDX(data); break;
+                case 0x6: DEC(address, accumulator); break;
+                case 0x7: INC(address, accumulator); break;
             }
         }
         
@@ -135,33 +228,70 @@ class NES{
     }
     void ORA(char data){
         A|=data;
+        checkValueFlags(A);
     }
 
     void AND(char data){
         A&=data;
+        checkValueFlags(A);
     }
     void EOR(char data){
         A^=data;
+        checkValueFlags(A);
     }
     void ADC(char data){
-        if(A+data>0xFF){
-            
-        }
-        
-
+        bool carryBit = A + data > 0xFF ? 1 : 0;
+        bool overFlowBit = (A + data > 0x7F || A + data < 0x80);
+        setFlag(CARRY, carryBit);
         A+=data;
+        checkValueFlags(A);
+        setFlag(OVERFLOW, overFlowBit);
     }
     void SBC(char data){
+        bool overFlowBit = (A - data > 0x7F || A - data < 0x80);
+        bool borrowBit = A >= data ? 1: 0;
         A-=data;
+        checkValueFlags(A);
+        setFlag(OVERFLOW, overFlowBit);
+        setFlag(CARRY, borrowBit);
     }
     void STA(short int address){
         memory[address] = A; // check this
     }
     void LDA(char data){
-        A = data;
+        A = data; //check if flag is to be set here
     }
     void CMP(char data){
+        bool borrowBit = A >= data ? 1: 0;
+        setFlag(CARRY, borrowBit);
+        checkValueFlags(A - data);
         // Update flags based on CMP
+    }
+
+    void ASL(unsigned short address, bool accumulator){
+
+    }
+    void ROL(unsigned short address, bool accumulator){
+        
+    }
+    void LSR(unsigned short address, bool accumulator){
+        
+    }
+    void ROR(unsigned short address, bool accumulator){
+        
+    }
+    void STX(unsigned short address, bool accumulator){
+        
+    }
+
+    void LDX(char data){
+        X = data; //check if flag to be set here
+    }
+    void DEC(unsigned short address, bool accumulator){
+        
+    }
+    void INC(unsigned short address, bool accumulator){
+        
     }
 };
 
