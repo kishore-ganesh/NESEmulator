4e319fce6e9b0dd889ea59fd68b622bc23262dd2
kishore-ganesh <kishore2912000@gmail.com>
2020-02-27 15:10:33 +0530

    Switched to textures
    Signed-off-by: kishore-ganesh <kishore2912000@gmail.com>

--- sdl_handler.cpp
+++ sdl_handler.cpp
@@ -19,16 +19,32 @@ void SDLHandler::handleEvent()
 
 void SDLHandler::displayFrame(std::vector<std::vector<RGB>> display)
 {
-    for (int x = 0; x < 256; x++)
+    // printf("FRAME \n");
+    unsigned char srcPixels[256*240*3];
+    int index = 0;
+    for (int y = 0; y < 240; y++)
     {
-        for (int y = 0; y < 240; y++)
+        for (int x = 0; x < 256; x++)
         {
             RGB pixel = display[x][y];
-            SDL_SetRenderDrawColor(renderer, pixel.r, pixel.g, pixel.b, 1);
-            SDL_RenderDrawPoint(renderer, x, y);
+            // printf("INDEX: %d\n", index);
+            srcPixels[index] = pixel.r;
+            srcPixels[index+1] = pixel.g;
+            srcPixels[index+2] = pixel.b;
+            index+=3;
+
+            // SDL_SetRenderDrawColor(renderer, pixel.r, pixel.g, pixel.b, 1);
+            // SDL_RenderDrawPoint(renderer, x, y);
         }
     }
+    void* destPixels;
+    int pitch;
+    SDL_LockTexture(texture, NULL, &destPixels, &pitch);
+    memcpy(destPixels, srcPixels, 256*240*3);
+    SDL_UnlockTexture(texture);
+    SDL_RenderCopy(renderer, texture, NULL, NULL ); //Update screen SDL_RenderPresent( gRenderer );
     SDL_RenderPresent(renderer);
+    // SDL_RenderPresent(renderer);
 }
 
 void SDLHandler::begin(){
--- sdl_handler.h
+++ sdl_handler.h
@@ -6,6 +6,7 @@ class SDLHandler
 {
     SDL_Window *window;
     SDL_Renderer *renderer;
+    SDL_Texture* texture;
     NES* nes;
     bool shouldQuit = false;
     public:
@@ -13,7 +14,8 @@ class SDLHandler
     {
         SDL_Init(SDL_INIT_VIDEO);
         window = SDL_CreateWindow("NESEmulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 256, 240, SDL_WINDOW_SHOWN);
-        renderer = SDL_CreateRenderer(window, -1, 0);
+        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB24, SDL_TEXTUREACCESS_STREAMING, 256, 240);
         this->nes = nes;
     }
 
